838699283a5ab9c2c7099df210ae4ac3638e9e3e
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-09-16 03:59:34 +0200

    Update MySQL CMake find script to work on Windows

--- cmake/FindMySql.cmake
+++ cmake/FindMySql.cmake
@@ -1,60 +1,49 @@
+# Source: https://gitlab.kitware.com/cmake/community/-/wikis/contrib/modules/FindMySQL
+
 # - Find mysqlclient
 # Find the native MySQL includes and library
 #
-#  MYSQL_INCLUDE_DIRS - where to find mysql.h, etc.
-#  MYSQL_LIBRARIES    - List of libraries when using MySQL.
-#  MYSQL_FOUND        - True if MySQL found.
+#  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
+#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
+#  MYSQL_FOUND       - True if MySQL found.
 
-if (NOT DEFINED MSVC)
-	if (USE_MARIADB)
-		find_path(MYSQL_INCLUDE_DIR
-			NAMES mysql.h
-			PATH_SUFFIXES mariadb
-			)
+IF (MYSQL_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(MYSQL_FIND_QUIETLY TRUE)
+ENDIF (MYSQL_INCLUDE_DIR)
 
-		find_library(MYSQL_LIBRARY
-			NAMES mariadb
-			PATH_SUFFIXES mariadb
-			)
-	else()
-		find_path(MYSQL_INCLUDE_DIR
-			NAMES mysql.h
-			PATH_SUFFIXES mysql
-			)
+FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
+  /usr/local/include/mysql
+  /usr/include/mysql
+)
 
-		find_library(MYSQL_LIBRARY
-			NAMES mysqlclient mysqlclient_r
-			PATH_SUFFIXES mysql
-			)
-	endif()
-else()
-	find_path(MYSQL_INCLUDE_DIR
-		NAMES mysql.h
-		PATH_SUFFIXES include
-		PATHS "$ENV{SystemDrive}/Program Files/mariadb-connector-c/include/mariadb"
-					"$ENV{SystemDrive}/Program Files (x86)/mariadb-connector-c/include/mariadb"
-		)
-	MESSAGE("INCLUDE_DIR: ${MYSQL_INCLUDE_DIR}")
+SET(MYSQL_NAMES mysqlclient mysqlclient_r)
+FIND_LIBRARY(MYSQL_LIBRARY
+  NAMES ${MYSQL_NAMES}
+  PATHS /usr/lib /usr/local/lib
+  PATH_SUFFIXES mysql
+)
 
-	find_library(MYSQL_LIBRARY
-		NAMES libmariadb
-		PATH_SUFFIXES lib
-		PATHS "$ENV{SystemDrive}/Program Files/mariadb-connector-c/lib/mariadb"
-					"$ENV{SystemDrive}/Program Files (x86)/mariadb-connector-c/lib/mariadb"
-		)
-	MESSAGE("LIB: ${MYSQL_LIBRARY}")
-endif()
+IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+  SET(MYSQL_FOUND TRUE)
+  SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
+ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+  SET(MYSQL_FOUND FALSE)
+  SET( MYSQL_LIBRARIES )
+ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MYSQL
-  FOUND_VAR MYSQL_FOUND
-  REQUIRED_VARS MYSQL_LIBRARY MYSQL_INCLUDE_DIR
-  )
+IF (MYSQL_FOUND)
+  IF (NOT MYSQL_FIND_QUIETLY)
+    MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
+  ENDIF (NOT MYSQL_FIND_QUIETLY)
+ELSE (MYSQL_FOUND)
+  IF (MYSQL_FIND_REQUIRED)
+    MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
+    MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
+  ENDIF (MYSQL_FIND_REQUIRED)
+ENDIF (MYSQL_FOUND)
 
-mark_as_advanced(
+MARK_AS_ADVANCED(
   MYSQL_LIBRARY
   MYSQL_INCLUDE_DIR
   )
-
-set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIR})
-set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
